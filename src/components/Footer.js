c17df36e4f6e5f2054d587db1ce1dd2186146796
Moetez Jlidi <moetez1@live.fr>
2024-10-30 11:57:05 +0100

    updating the linkedin link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/moetez-j-6041781ab/"
+                href="https://www.linkedin.com/in/moetezjlidi/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
